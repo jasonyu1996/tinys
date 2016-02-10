a11b363f4cf6c5fcb1e5f605583c3910537fdd61
Jason Yu <yzjc15@mails.tsinghua.edu.cn>
2016-02-10 16:55:37 +0800

    attribute operation added

--- tinys.js
+++ tinys.js
@@ -1,4 +1,20 @@
 (function(){
+    var TNode = function(raw){
+        this.raw = raw;
+        TNode.prototype.attr = function(attrName, val){
+            if(!attrName)
+                return undefined;
+            if(val)
+                this.raw.setAttribute(attrName, val.toString());
+            else{
+                var res = this.raw.getAttribute(attrName);
+                if(res)
+                    return res;
+                else
+                    return undefined;
+            }
+        };
+    };
     window.$ = function(sel){
         var res;
         if(sel.charAt(0) == '#')
@@ -7,13 +23,22 @@
             res = document.getElementsByClassName(sel.substr(1, sel.length));
         else
             res = document.getElementsByTagName(sel);
-        if(res && res.isArray){
+        if(res && res instanceof HTMLCollection){
             if(res.length == 0)
                 res = null;
             else if(res.length == 1)
                 res = res[0];
         }
+        if(res){
+            if(res instanceof HTMLCollection){
+                var tmp = [];
+                for(var i = 0; i < res.length;i ++)
+                    tmp[i] = new TNode(res[i]);
+                res = tmp;
+            } else
+                res = new TNode(res);
+        }
         return res;
-    }
+    };
 })();
 
